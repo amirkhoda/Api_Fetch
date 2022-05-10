6558c0d02e7634e075c8507ba92c664c265cdb15
amirkhoda8 <amir.hosein.clan@gmail.com>
2022-05-10 13:36:25 +0430

    test3 vs GIT lens plugin

--- src/Data.jsx
+++ src/Data.jsx
@@ -4,7 +4,8 @@ import { Button } from 'react-bootstrap'
 
 function Data(props) {
     const [data, setData] = useState([])
-    const [searchTerm, setSearchTerm] = useState("");
+    const [minIndex, setMinIndex] = useState(0)
+    const [maxIndex, setMaxIndex] = useState(0)
 
 
     useEffect(() => {
@@ -15,18 +16,19 @@ function Data(props) {
             }).catch(err => console.log(err))
     }, [])
 
-    const arr = data.map((data, index) => {
-        if (searchTerm == data.id) {
-            return (
-                <div>
-                    <table class="table table-bordered table-success ">
-                        <tr>
-                            <th>{data.id}</th>
-                            <th>{data.title}</th>
+    // for (let i = 0; i < array.length; i++) {
+    //     const element = array[i];
 
-                        </tr>
-                    </table>
-                </div>
+    // }
+
+    const arr = data.map((data, index) => {
+        if (minIndex <= (data.id) && (data.id) <= (maxIndex)) {
+            // console.log(`${minIndex} : ${data.id} : ${maxIndex}`)
+            return (
+                <tr>
+                    <td>{data.id}</td>
+                    <td>{data.title}</td>
+                </tr>
             )
 
         }
@@ -34,28 +36,41 @@ function Data(props) {
     })
 
     return (
-        <>
-            <div className=" text-center mt-5 m-4" >
+        <body>
+            <div className="text-center align-items-center mt-5 m-4  " >
                 <form>
-                    <input type="text" name="id" placeholder="id:" onChange={(event) => {
-                        setSearchTerm(event.target.value);
-                    }} />
+                    <div>
+                        <label className="me-4">MIN :</label>
+                        <input type="text" name="id" placeholder="Enter Your min number:" onChange={(event) => {
+                            setMinIndex((event.target.value));
+                        }} />
+                    </div>
+                    <div>
+                        <label className="me-4 mt-4 ">Max :</label>
+                        <input type="text" name="id" placeholder="Enter Your max number:" onChange={(event) => {
+                            setMaxIndex((event.target.value));
+                        }} />
+                    </div>
+
+                    <div class="form-text ms-3 mt-2">
+                        Between 1-200 number
+                    </div>
                 </form>
                 <h1 className="mt-5">use axios with react js</h1>
-                <table class="table table-bordered table-success table-hover w-50  ">
-
-                    <tr>
-                        <th >Id</th>
-                        <th >Title</th>
-
-                    </tr>
-                    {arr}
-
-
+                <table class="table table-dark w-50 ms-auto me-auto mt-5 ">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Text</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {arr}
+                    </tbody>
                 </table>
-
             </div>
-        </>
+
+        </body>
     )
 }
 
