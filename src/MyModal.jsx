cf5dbf9ea354a24109b1fa9df7d3568b0a9dc2fb
amirkhoda8 <amir.hosein.clan@gmail.com>
2022-06-01 19:28:23 +0430

    age&gender

--- src/MyModal.jsx
+++ src/MyModal.jsx
@@ -3,10 +3,13 @@ import { Modal, Button, ModalBody, ModalHeader, ModalFooter } from 'react-bootst
 import { Label,Input } from 'reactstrap'
 import LoginModal from './LoginModal'
 import SignUp from './SignUp'
+import './App.css';
 
 
 function MyModal(props) {
     const [showLogin, setShowLogin] = useState(true)
+
+    
 
     function MouseEnterFooterModal(e) {
         e.target.style.backgroundColor = '#ea4c89'
@@ -24,26 +27,27 @@ function MyModal(props) {
         else {
             return (
                 <SignUp />
+                
             )
         }
     }
     return (
         <Modal {...props} contentClassName="bg-dark" >
             <ModalHeader className="text-white">
-                <div style={{marginLeft:"3rem"}}>
-                <Input id="tab1" type="radio" name="tabs" style={{color: "inherit", textDecoration: "none",marginTop:"0.7rem",display:"none",border:"0",borderRadius:"0",padding:"0",lineHeight:"normal",fontFamily:"inherit",boxSizing:"border-box",backgroundColor:"initial",cursor:"default",appearance:"auto",writingMode:"horizontal-tb",textRendering:"auto",color:"fieldtext",letterSpacing:"normal",wordSpacing:"normal",textTransform:"none",textIndent:"0px",textShadow:"none",textAlign:"start"}} onClick={() => { setShowLogin(true) }}/>
-                <Label class="tab-lable" for="tab1"style={{fontSize:"1.5rem",color:"#555"}} >Login</Label>
+                <div  style={{marginLeft:"3rem"}} >
+                <input id="tab1" type="radio" name="tabs"  style={{display:"none"}} onClick={() => { setShowLogin(true) }}/>
+                <label class="tab_lable" for="tab1"style={{fontSize:"1.5rem",color:"#555"}} >Login</label>
                 </div>
                 <div style={{marginRight:"3rem"}}>
-                <Input id="tab2" type="radio" name="tabs" style={{ marginLeft:"12rem", color: "inherit", textDecoration: "none",marginTop:"0.7rem",display:"none",border:"0",borderRadius:"0",padding:"0",lineHeight:"normal",fontFamily:"inherit",boxSizing:"border-box",backgroundColor:"initial",cursor:"default",appearance:"auto",writingMode:"horizontal-tb",textRendering:"auto",color:"fieldtext",letterSpacing:"normal",wordSpacing:"normal",textTransform:"none",textIndent:"0px",textShadow:"none",textAlign:"start"}} onClick={() => { setShowLogin(false) }}/>
-                <Label class="tab-lable" for="tab2" style={{fontSize:"1.5rem",color:"#555"}}>signup</Label>
+                <Input id="tab2" type="radio" name="tabs" style={{ display:"none"}} onClick={() => { setShowLogin(false) }}/>
+                <Label class="tab_lable" for="tab2" style={{fontSize:"1.5rem",color:"#555"}}>signup</Label>
                 </div>
             </ModalHeader>
             <ModalBody>
                 {showLoginModal()}
             </ModalBody>
             <ModalFooter>
-                <Button onMouseEnter={MouseEnterFooterModal} onMouseLeave={MouseLeaveFpoterModal} style={{ backgroundColor: "#BAB9B9" }} onClick={props.onHide}>Okay</Button>
+                <Button onMouseEnter={MouseEnterFooterModal} onMouseLeave={MouseLeaveFpoterModal} style={{ backgroundColor: "#BAB9B9",border:"none" }} onClick={props.onHide}>Okay</Button>
             </ModalFooter>
         </Modal>
     )
--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -25,9 +25,11 @@ function SignUp() {
             <Form className="form">
                 <FormGroup>
                     <Label className="text-white mb-2" for="exampleEmail">Username</Label>
-                    <Input
-                        type="email"
-                        name="email"
+                    <Input         
+                        id="exampleEmail"
+                        placeholder="Enter Your Email"
+                    />
+                     <Input
                         id="exampleEmail"
                         placeholder="Enter Your Email"
                     />
@@ -42,16 +44,63 @@ function SignUp() {
                     />
                     <Label className="text-white mt-2" ><a href="">Forget Password?</a></Label>
                 </FormGroup>
-                <FormGroup className="mt-4">
-                    <Label className="text-white mb-2" for="examplePassword">Password</Label>
-                    <Input
-                        type="password"
-                        name="password"
-                        id="examplePassword"
-                        placeholder="Enter Your Password"
-                    />
-                    <Label className="text-white mt-2" ><a href="">Forget Password?</a></Label>
-                </FormGroup>
+                
+                    <form style={{marginTop:"2rem"}}>
+                        <label  className="text-white" >Age:</label>
+                        <select style={{height:"2rem",marginLeft:"1rem"}}  placeholder="Age">
+                                <option value="age"> Enter Your age</option>
+                                <option value="1">10</option>
+                                <option value="2">1</option>
+                                <option value="3">12</option>
+                                <option value="3">13</option>
+                                <option value="3">14</option>
+                                <option value="3">15</option>
+                                <option value="3">16</option>
+                                <option value="3">17</option>
+                                <option value="3">18</option>
+                                <option value="3">19</option>
+                                <option value="3">20</option>
+                                <option value="3">21</option>
+                                <option value="3">22</option>
+                                <option value="3">23</option>
+                                <option value="3">24</option>
+                                <option value="3">25</option>
+                                <option value="3">26</option>
+                                <option value="3">27</option>
+                                <option value="3">28</option>
+                                <option value="3">29</option>
+                                <option value="3">30</option>
+                                <option value="3">31</option>
+                                <option value="3">32</option>
+                                <option value="3">33</option>
+                                <option value="3">34</option>
+                                <option value="3">35</option>
+                                <option value="3">36</option>
+                                <option value="3">37</option>
+                                <option value="3">38</option>
+                                <option value="3">39</option>
+                                <option value="3">40</option>
+                                <option value="3">42</option>
+                                <option value="3">43</option>
+                                <option value="3">44</option>
+                                <option value="3">45</option>
+                                <option value="3">46</option>
+                                <option value="3">47</option>
+                                <option value="3">48</option>
+                                <option value="3">49</option>
+                                <option value="3">50</option>
+                        </select> 
+                        <label style={{marginLeft:"7rem"}} className="text-white" >Gender:</label>
+                        <select style={{height:"2rem",marginLeft:"1rem"}}>
+                                <option value="age">Gender</option>
+                                <option value="1">Male</option>
+                                <option value="2">Female</option>
+                                <option value="3">other</option>
+                        </select>
+                        
+                    </form>
+                   
+            
                 <div class="text-center">
                     <Button onMouseEnter={MouseEnterSubmit} onMouseLeave={MouseLeaveSubmit} className="text-white " style={{ backgroundColor: "#ea4c89", border: "none", marginTop: "2rem", width: "5rem", height: "3rem", borderRadius: "15px" }}>Submit</Button>
                 </div>
