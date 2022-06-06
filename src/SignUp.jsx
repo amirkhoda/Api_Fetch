549ca9c49f1492d6597f6a89251bbf24035c6c79
amirkhoda8 <amir.hosein.clan@gmail.com>
2022-06-06 18:41:42 +0430

    login_newVersion

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -23,108 +23,91 @@ function SignUp() {
     return (
         <div>
             <Form className="form">
-                //name
-                <FormGroup>
-                    <Label className="text-white mb-2" >Name</Label>
-                    <Input               
-                        placeholder="Enter Your Name"
-                    />
-                </FormGroup>
-                //lastName
-                <FormGroup >
-                    <Label className="text-white mb-2">LastName</Label>
-                    <Input         
-                        placeholder="Enter Your LastName"
-                    />
-                </FormGroup>
-                //email
-                <FormGroup>
-                    <Label className="text-white mb-2">Email</Label>
-                    <Input
-                        placeholder="Enter Your Email"
-                    />
-                </FormGroup>
-                //phone
-                <FormGroup>
-                <Label className="text-white mb-2" >Phone</Label>
-                    <Input             
-                        placeholder="Enter Your Phone: +98"
-                    />
-                </FormGroup>
-                //username
-                <FormGroup>
-                    <Label className="text-white mb-2" >UserName</Label>
-                    <Input
-                        placeholder="Enter Your UserName"
-                    />
-                </FormGroup>
-                //password
-                <FormGroup >
-                    <Label className="text-white mb-2" >Password</Label>
-                    <Input
-                        placeholder="Enter Your Password"
-                    />
-                </FormGroup>
-                
-                    <form style={{marginTop:"2rem"}}>
-                        <label  className="text-white" >Age:</label>
-                        <select style={{height:"2rem",marginLeft:"1rem"}}  placeholder="Age">
-                                <option value="age"> Enter Your age</option>
-                                <option value="1">10</option>
-                                <option value="2">1</option>
-                                <option value="3">12</option>
-                                <option value="3">13</option>
-                                <option value="3">14</option>
-                                <option value="3">15</option>
-                                <option value="3">16</option>
-                                <option value="3">17</option>
-                                <option value="3">18</option>
-                                <option value="3">19</option>
-                                <option value="3">20</option>
-                                <option value="3">21</option>
-                                <option value="3">22</option>
-                                <option value="3">23</option>
-                                <option value="3">24</option>
-                                <option value="3">25</option>
-                                <option value="3">26</option>
-                                <option value="3">27</option>
-                                <option value="3">28</option>
-                                <option value="3">29</option>
-                                <option value="3">30</option>
-                                <option value="3">31</option>
-                                <option value="3">32</option>
-                                <option value="3">33</option>
-                                <option value="3">34</option>
-                                <option value="3">35</option>
-                                <option value="3">36</option>
-                                <option value="3">37</option>
-                                <option value="3">38</option>
-                                <option value="3">39</option>
-                                <option value="3">40</option>
-                                <option value="3">42</option>
-                                <option value="3">43</option>
-                                <option value="3">44</option>
-                                <option value="3">45</option>
-                                <option value="3">46</option>
-                                <option value="3">47</option>
-                                <option value="3">48</option>
-                                <option value="3">49</option>
-                                <option value="3">50</option>
-                        </select> 
-                        <label style={{marginLeft:"7rem"}} className="text-white" >Gender:</label>
-                        <select style={{height:"2rem",marginLeft:"1rem"}}>
-                                <option value="age">Gender</option>
-                                <option value="1">Male</option>
-                                <option value="2">Female</option>
-                                <option value="3">other</option>
-                        </select>
-                        
-                    </form>
-                   
-            
-                <div class="text-center">
-                    <Button onMouseEnter={MouseEnterSubmit} onMouseLeave={MouseLeaveSubmit} className="text-white " style={{ backgroundColor: "#ea4c89", border: "none", marginTop: "2rem", width: "5rem", height: "3rem", borderRadius: "15px" }}>Submit</Button>
+                {/* name */}
+                <div style={{ marginTop: "1rem" }}>
+                    <label className="text-white" style={{ fontSize: "16px" }}>Name : </label>
+                    <input style={{ borderRadius: "10px", marginLeft: "8px" }} size="15" placeholder="Enter Your Name" />
+                    {/* lastName */}
+                    <label className="text-white" style={{ fontSize: "16px", marginLeft: "1rem" }}>LastName : </label>
+                    <input style={{ borderRadius: "10px", marginLeft: "8px" }} size="15" placeholder="Enter Your LastName" />
                 </div>
+
+                {/* email */}
+                <div style={{ marginTop: "2rem" }}>
+                    <label className="text-white" style={{ fontSize: "16px" }}>Email : </label>
+                    <input style={{ borderRadius: "10px", marginLeft: "12px" }} size="15" placeholder="Enter Your Email" />
+                    {/* phone */}
+                    <label className="text-white" style={{ fontSize: "16px", marginLeft: "2rem" }}>Phone : </label>
+                    <input style={{ borderRadius: "10px", marginLeft: "15px" }} size="15" placeholder="Enter Your phone: +98" />
+                </div>
+                {/* username */}
+                <div style={{ marginTop: "2rem" }}>
+                    <label className="text-white" style={{ fontSize: "16px" }}>UserName : </label>
+                    <input style={{ borderRadius: "10px", marginLeft: "12px" }} size="20"  placeholder="UserName" />
+
+                    <label  className="text-white" style={{marginLeft:"1.7rem"}}>Gender:</label>
+                    <select style={{ height: "2rem", marginLeft: "1rem",borderRadius:"10px" }}>
+                        <option value="age">Gender</option>
+                        <option value="1">Male</option>
+                        <option value="2">Female</option>
+                        <option value="3">other</option>
+                    </select>
+                </div>
+                {/* password */}
+                <div style={{ marginTop: "2rem" }}>
+                    <label className="text-white" style={{ fontSize: "16px"}}>Password : </label>
+                    <input style={{ borderRadius: "10px", marginLeft: "15px" }} size="20" placeholder="Password" />
+
+                    <label className="text-white" style={{marginLeft:"2.7rem"}} >Age:</label>
+                    <select style={{ height: "2rem", marginLeft: "1rem" ,borderRadius:"10px"}}  placeholder="Age">
+                        <option value="age">Your age</option>
+                        <option value="1">10</option>
+                        <option value="2">1</option>
+                        <option value="3">12</option>
+                        <option value="3">13</option>
+                        <option value="3">14</option>
+                        <option value="3">15</option>
+                        <option value="3">16</option>
+                        <option value="3">17</option>
+                        <option value="3">18</option>
+                        <option value="3">19</option>
+                        <option value="3">20</option>
+                        <option value="3">21</option>
+                        <option value="3">22</option>
+                        <option value="3">23</option>
+                        <option value="3">24</option>
+                        <option value="3">25</option>
+                        <option value="3">26</option>
+                        <option value="3">27</option>
+                        <option value="3">28</option>
+                        <option value="3">29</option>
+                        <option value="3">30</option>
+                        <option value="3">31</option>
+                        <option value="3">32</option>
+                        <option value="3">33</option>
+                        <option value="3">34</option>
+                        <option value="3">35</option>
+                        <option value="3">36</option>
+                        <option value="3">37</option>
+                        <option value="3">38</option>
+                        <option value="3">39</option>
+                        <option value="3">40</option>
+                        <option value="3">42</option>
+                        <option value="3">43</option>
+                        <option value="3">44</option>
+                        <option value="3">45</option>
+                        <option value="3">46</option>
+                        <option value="3">47</option>
+                        <option value="3">48</option>
+                        <option value="3">49</option>
+                        <option value="3">50</option>
+                    </select>
+                </div>
+
+                <div class="text-center">
+                    <Button onMouseEnter={MouseEnterSubmit} onMouseLeave={MouseLeaveSubmit} className="text-white " style={{ backgroundColor: "#ea4c89", border: "none", marginTop: "2rem", width: "5rem", height: "3rem", borderRadius: "15px" }}>SignUp</Button>
+                </div>
+                
                 <Button onMouseEnter={MouseEnterrModal} onMouseLeave={MouseLeaveModal} className="text-white" style={{ backgroundColor: "#BAB9B9", border: "none", marginTop: "2rem", width: "29rem", height: "3rem" }}><img src="../assets/GitHub.png" style={{ height: "2rem", marginRight: "1rem" }} />  Sign in With Google</Button>
                 <Button onMouseEnter={MouseEnterrModal} onMouseLeave={MouseLeaveModal} className="text-white" style={{ backgroundColor: "#BAB9B9", border: "none", marginTop: "2rem", width: "29rem", height: "3rem" }}><img src="../assets/google.png" style={{ height: "2rem", marginRight: "1rem" }} />  Sign in With GitHub</Button>
 
